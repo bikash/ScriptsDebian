a97b9ef2b493ec417d62d0a3f417dd845d234afb
bikash <er.bikash21@gmail.com>
2016-03-11 02:24:45 -0500

    caffe installation script.

--- install_caffe_cpu.sh
+++ install_caffe_cpu.sh
@@ -27,11 +27,10 @@ sudo apt-get update && apt-get install -y \
   unzip \
   wget
 
-
-  update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-4.6 30 && \
-  update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-4.6 30 && \
-  update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 30 && \
-  update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 30
+sudo  update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-4.6 30 && \
+sudo  update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-4.6 30 && \
+sudo  update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 30 && \
+sudo  update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 30
 
 
 cd /opt && git clone https://github.com/BVLC/caffe.git
@@ -42,11 +41,12 @@ cd /opt && git clone https://github.com/BVLC/caffe.git
 # Glog
 cd /opt && wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz && \
   tar zxvf glog-0.3.3.tar.gz && \
-  cd /opt/glog-0.3.3 && \
+  cd glog-0.3.3 && \
   ./configure && \
-  make && \
-  make install
+  make 
+sudo  make install
 
+sudo ldconfig
 
 # Gflags
 cd /opt && \
@@ -58,5 +58,38 @@ cd /opt && \
   export CXXFLAGS="-fPIC" && \
   cmake .. && \
   make VERBOSE=1 && \
-  make && \
-  make install
+  make
+sudo make install
+
+
+sudo apt-get install -y libgflags-dev
+
+# Build Caffe core
+cd /opt/caffe && \
+cp Makefile.config.example Makefile.config && \
+echo "CPU_ONLY := 1" >> Makefile.config && \ 
+echo "CXX := /usr/bin/g++-4.6" >> Makefile.config && \
+sed -i 's/CXX :=/CXX ?=/' Makefile 
+sudo  make all
+
+# Add ld-so.conf so it can find libcaffe.so
+caffe-ld-so.conf /etc/ld.so.conf.d/
+
+# Run ldconfig again (not sure if needed)
+sudo ldconfig
+
+# Install python deps
+cd /opt/caffe && \
+cat python/requirements.txt | xargs -L 1 sudo pip install
+
+# Numpy include path hack - github.com/BVLC/caffe/wiki/Ubuntu-14.04-VirtualBox-VM
+ln -s /usr/include/python2.7/ /usr/local/include/python2.7 && \
+ln -s /usr/local/lib/python2.7/dist-packages/numpy/core/include/numpy/ /usr/local/include/python2.7/numpy
+
+# Build Caffe python bindings
+cd /opt/caffe && make pycaffe
+
+
+# Make + run tests
+cd /opt/caffe && make test && make runtest
+
